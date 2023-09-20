6aefa34f5091eb8c649400f972d5d49275b9be62
AaryaDesai1 <143753050+AaryaDesai1@users.noreply.github.com>
2023-09-20 17:01:00 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -40,26 +40,3 @@ visualize_data(
     xlabel="Socioeconomic Status",
     ylabel="Risk Preferences",
 )
-
-'''Testing Valid Input'''
-
-def testing_main_avg_input():
-    assert average(None) == "Please provide valid DataFrame type input."
-
-
-def testing_main_med_input():
-    assert med(None) == "Please provide valid DataFrame type input."
-
-
-def testing_main_std_input():
-    assert standard_deviation(None) == "Please provide valid DataFrame type input."
-
-
-def testing_main_summary_input():
-    assert summary_stats(None) == "Please provide valid DataFrame type input."
-
-
-def testing_main_visualize_input():
-    assert (
-        visualize_data(None, None, None) == "Please provide valid DataFrame type input."
-    )
